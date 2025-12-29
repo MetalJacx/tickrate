04f7f439a6584f86f83270e179ca899b70920775
Joe Benga <jbenga.iii@gmail.com>
2025-12-29 12:28:17 -0700

    combat log

--- js/util.js
+++ js/util.js
@@ -3,7 +3,7 @@ import { state } from "./state.js";
 export function addLog(msg, type = "normal") {
   const time = new Date().toLocaleTimeString();
   state.log.unshift({ text: `[${time}] ${msg}`, type });
-  if (state.log.length > 50) state.log.pop();
+  if (state.log.length > 1000) state.log.pop();
 }
 
 export function randInt(maxExclusive) {
