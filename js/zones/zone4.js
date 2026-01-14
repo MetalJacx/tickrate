af0786b1af449e32e2273b8b4fed4fc01c0e964e
Joe Benga <jbenga.iii@gmail.com>
2026-01-13 23:20:55 -0700

    feat: adjust enemy weights for zones 1 to 6 to enhance loot drop dynamics

--- js/zones/zone4.js
+++ js/zones/zone4.js
@@ -19,7 +19,7 @@ export default {
   enemies: [
     { id: "hill_skirmisher", weight: 1.0, loot: [{ itemId: "rusty_spear", dropRate: 0.05 }] },
     { id: "dark_wolf", weight: 0.9, loot: [{ itemId: "health_potion_small", dropRate: 0.06 }] },
-    { id: "captain_arvok", weight: 0.001, loot: [{ itemId: "hill_captains_blade", dropRate: 0.35 }, { itemId: "ridgewatch_vest", dropRate: 0.25 }, { itemId: "arvok_signet", dropRate: 0.28 }] },
+    { id: "captain_arvok", weight: 0.034, loot: [{ itemId: "hill_captains_blade", dropRate: 0.35 }, { itemId: "ridgewatch_vest", dropRate: 0.25 }, { itemId: "arvok_signet", dropRate: 0.28 }] },
     { id: "ridge_bandit", weight: 0.8, loot: [{ itemId: "ridge_stone", dropRate: 0.12, minQty: 1, maxQty: 2 }] },
     { id: "rock_scrabbler", weight: 0.7, loot: [{ itemId: "ridge_stone", dropRate: 0.25, minQty: 1, maxQty: 3 }] }
   ],
